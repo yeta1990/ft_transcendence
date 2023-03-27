23bdd3bd24d81110a4a494bc9c849dc97965152d
Alberto Garcia lopez <albgarci@student.42madrid.com>
2023-03-27 20:38:43 +0200

    Feature: installing angular cli

---
+++ front/install_angular_dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+cd /usr/src/app
+
+export NG_CLI_ANALYTICS="false"
+
+npm install -g npm@9.6.2
+echo 'N' | npm install -g @angular/cli
+ng analytics off
+if [ ! -d ./my-app ]
+then
+	echo "yeee"
+	ng new my-app --defaults
+fi
+
+cd my-app
+
+ng serve --host 0.0.0.0 --port 4200 -y
+
+tail -f /dev/null
---
+++ scripts/install_angular_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /usr/src/app
+
+npm install -g @angular/cli
+ng new angulardockerproject
