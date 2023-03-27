d4dc44d1ea98206e10036c91c4806f5f59a49c59
albgarci <albgarci@student.42madrid.com>
2023-03-28 00:13:38 +0200

    Good config for angular

--- front/install_angular_dependencies.sh
+++ front/install_angular_dependencies.sh
@@ -4,13 +4,9 @@ cd /usr/src/app
 export NG_CLI_ANALYTICS="false"
 
 npm install -g npm@9.6.2
-echo 'N' | npm install -g @angular/cli
+npm install -g @angular/cli
 ng analytics off
-if [ ! -d ./my-app ]
-then
-	ng new my-app --defaults
-fi
 
-cd my-app
-
+cd srcs
+npm install
 ng serve --host 0.0.0.0 --port 4200
