2f42854707a355be132c4e5a35a65bee3f073d32
albgarci <albgarci@student.42madrid.com>
2023-03-29 01:30:11 +0200

    Playground: user endpoint created

---
+++ srcs/src/user/user.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('users')
+export class UserController {
+	@Get()
+	findAll(): string{
+		return "All users"
+	}
+	@Get(':id')
+	findOne(@Param() params){
+		return {
+			id: params.id,
+			name: "pepe"	
+		}
+	}
+
+}
