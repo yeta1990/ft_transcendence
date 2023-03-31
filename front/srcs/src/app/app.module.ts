44b3c920402528969775916d117d05d4f6bef696
albgarci <albgarci@student.42madrid.com>
2023-03-31 12:37:05 +0200

    Feature: simple form showing its values in a json

--- front/srcs/src/app/app.module.ts
+++ front/srcs/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
@@ -10,6 +11,7 @@ import { FormComponent } from './form/form.component';
     FormComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
--- front/srcs/src/app/form/form.component.ts
+++ front/srcs/src/app/form/form.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-
+	name = "";
+	email = "";
+	
 }
