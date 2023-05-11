f5ed8ba0443691bac28eb81f23a8d93ee43160cd
Pablo Otero <potero91@gmail.com>
2023-05-11 17:00:20 +0200

    merge solved

--- frontend/src/src/environments/environment.ts
+++ frontend/src/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-   apiBaseUrl: 'http://localhost:3000',
-   apiUrl: 'http://localhost:3000',
-   frontendUrl: 'http://localhost:4200',
-   clientId42: 'u-s4t2ud-8a6f2fa2b390840637041ee38b7e930881c496975d8123b200f71aac854c7c02'
+   apiBaseUrl: 'http://10.11.14.1:3000',
+   apiUrl: 'http://10.11.14.1:3000',
+   frontendUrl: 'http://10.11.14.1:4200',
+   clientId42: 'u-s4t2ud-f2685b7b768e9937e6ec51d01b547b18b48a8d1a2e703ceb6017a25f635e3042'
 };
