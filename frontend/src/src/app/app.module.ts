34607cda299ee48ebd9628e8406ce88abb8f195b
albgarci <albgarci@student.42madrid.com>
2023-05-13 13:32:17 +0200

    Feature: Error interceptor chain

--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -7,9 +7,8 @@ import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { LoginComponent } from './login/login.component';
-import {AuthInterceptor } from './auth.interceptor';
 import { CallbackComponent } from './callback/callback.component';
-import { ErrorInterceptor } from './error-interception/error.interceptor';
+import { interceptorProviders } from './app.interceptors';
 
 @NgModule({
   declarations: [
@@ -27,16 +26,7 @@ import { ErrorInterceptor } from './error-interception/error.interceptor';
     AppRoutingModule
   ],
   providers: [
-  {
-	provide: HTTP_INTERCEPTORS,
-	useClass: AuthInterceptor,
-	multi: true
-  },
-  {
-    provide: HTTP_INTERCEPTORS,
-    useClass: ErrorInterceptor,
-    multi: true
-    },
+	interceptorProviders
   ],
   bootstrap: [AppComponent]
 })
--- frontend/src/src/app/error-interception/error.interceptor.ts
+++ frontend/src/src/app/error-interception/error.interceptor.ts
@@ -16,15 +16,16 @@ export class ErrorInterceptor implements HttpInterceptor {
     private authservice: AuthService
   ) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).pipe(catchError(err => {
       if ([401, 403].includes(err.status)) {
           this.authservice.logout();
       }
       //Add here future catch errors if...()
 
+		console.log("Catched HTTP error!");
       const error = err.error?.message || err.statusText;
-      console.error(err);
+     // console.error(err);
       return throwError(() => new Error(error));
     }))
   }
