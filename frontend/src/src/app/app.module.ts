c36d22aa4da18b4114b8cc91718fafc5639e9f67
albgarci <albgarci@student.42madrid.com>
2023-05-15 00:53:57 +0200

    Feature: socket.io integrated in front and back, connecting properly. Front doesn't manage received messages yet

--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing-module/app-routing-module.module'
+
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { LoginComponent } from './login/login.component';
 import { CallbackComponent } from './callback/callback.component';
 import { interceptorProviders } from './app.interceptors';
+import { ChatComponent } from './chat/chat.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+import { environment } from '../environments/environment'
+
+const socketConfig: SocketIoConfig = { url: environment.apiUrl, options: {} };
 
 @NgModule({
   declarations: [
@@ -16,14 +23,16 @@ import { interceptorProviders } from './app.interceptors';
     FormComponent,
     MyProfileComponent,
     LoginComponent,
-    CallbackComponent
+    CallbackComponent,
+    ChatComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocketIoModule.forRoot(socketConfig)
   ],
   providers: [
 	interceptorProviders
