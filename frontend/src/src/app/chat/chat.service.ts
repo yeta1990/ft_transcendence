b0a1d2cb6ca80e0ce5099e7323f76fa3276f5f06
Alberto Garcia lopez <albgarci@student.42madrid.com>
2023-05-15 20:16:53 +0200

    Feature: changed frontend socket.io library to the official one. Frontend: socket service separated from the component and its service. Backend: base gateway created to be extended from the specific gateways

--- frontend/src/src/app/chat/chat.service.ts
+++ frontend/src/src/app/chat/chat.service.ts
@@ -1,31 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
-import { map } from 'rxjs/operators';
+import { Subject, from } from  'rxjs';
+import { io } from "socket.io-client";
+import { environment } from '../../environments/environment'
 
-@Injectable({
-  providedIn: 'root'
-})
 export class ChatService {
-
-//https://github.com/rodgc/ngx-socket-io/issues/25#issuecomment-479936465
-  constructor(private socket: Socket) { 
-  	//defining namespace where this socket client will listen to
-	this.socket.ioSocket.nsp = '/chat'
-  }
-
-  //emit has two parameters:
-  // - category of the message
-  // - message
-  //the backend will handle each message depending on its category
-  // (see the chat.gateway, for insance)
-  sendMessage(msg: string) {
-	this.socket.emit('message', msg);
-  }
- 
-  getMessage(){
-	return this.socket
-		.fromEvent('message')
-		.pipe(map((data: any) => data))
-  }
-
+//https://auth0.com/blog/real-time-charts-using-angular-d3-and-socket-io/
+	constructor() {
+	}
 }
