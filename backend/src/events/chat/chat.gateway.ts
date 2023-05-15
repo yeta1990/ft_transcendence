d208c381f5f30f266edbe9390acee892c1e0bff2
Alberto Garcia lopez <albgarci@student.42madrid.com>
2023-05-15 16:09:02 +0200

    Feature: added logger in chat gateway, simplified cors config

--- backend/src/events/chat/chat.gateway.ts
+++ backend/src/events/chat/chat.gateway.ts
@@ -1,16 +1,32 @@
-import {   MessageBody, WebSocketServer, SubscribeMessage, WebSocketGateway , WsResponse} from '@nestjs/websockets';
+import { MessageBody, 
+	WebSocketServer, 
+	SubscribeMessage, 
+	WebSocketGateway, 
+	WsResponse,
+	OnGatewayInit,
+} from '@nestjs/websockets';
 import { Server } from 'socket.io';
 import { map } from 'rxjs/operators';
 import { from, Observable } from 'rxjs';
+import { Logger } from '@nestjs/common';
 
-//@WebSocketGateway({ cors: { origin: [process.env.FRONTEND_IP, 'http://localhost:3000', 'http://localhost:4200']}}
-@WebSocketGateway({ namespace: '/chat', cors: { origin: [process.env.FRONTEND_IP, 'http://localhost:3000', 'http://localhost:4200']}}
-)
+//https://stackoverflow.com/questions/69435506/how-to-pass-a-dynamic-port-to-the-websockets-gateway-in-nestjs
+
+@WebSocketGateway({ namespace: '/chat', cors: true } )
+//@WebSocketGateway({ namespace: '/chat', cors: { origin: [process.env.FRONTEND_IP, 'http://localhost:3000', 'http://localhost:4200']}})
+
 //https://github.com/nestjs/nest/blob/master/sample/02-gateways/src/events/events.gateway.ts
-export class ChatGateway {
+//@WebSocketGateway()
+export class ChatGateway implements OnGatewayInit {
+
+  private readonly logger = new Logger(ChatGateway.name)
 
   @WebSocketServer()
   server: Server;
+
+  afterInit(): void {
+	this.logger.log('Chat gateway initialized');
+  }
 
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): any {
