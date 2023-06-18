daef3354b55b66ede34dc4726ded2fc6df1eaaa8
Alberto <yeta1990@gmail.com>
2023-06-19 00:08:22 +0200

    Fix: chat wasn't loading previous room messages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 import { AuthModule } from './auth/auth.module'
 
