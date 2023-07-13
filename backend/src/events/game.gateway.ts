90267a93c2d37154fe15e1523bbc8417b8acb629
Pablo Otero <potero91@gmail.com>
2023-07-13 19:00:52 +0200

    back room game

--- backend/src/events/game.gateway.ts
+++ backend/src/events/game.gateway.ts
@@ -2,6 +2,7 @@ import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import { BaseGateway } from './base.gateway';
 import { Socket } from 'socket.io';
 import { ChatMessage, SocketPayload } from '@shared/types';
+import { RoomMessages, ChatUser } from '@shared/types';
 
 @WebSocketGateway({ namespace: '/game', cors: true } )
 export class GameGateway extends BaseGateway {
@@ -27,5 +28,53 @@ export class GameGateway extends BaseGateway {
   handleDown(client: Socket, payload: ChatMessage) {
     console.log("Going down\n");
     return { event: 'getSignal', data: 1 };
+  }
+
+  @SubscribeMessage('join')
+  async handleJoinRoom(client: Socket, rooms: string): Promise<void>{
+//  	  console.log("join message received: " + rooms);
+	  const splittedRooms: Array<string> = rooms.split(" ", 1)[0].split(",");
+	  const pass: string | undefined = rooms.split(" ")[1];
+	  let lastJoinedRoom: string;
+	  const adapter: any = this.server.adapter;
+	  const roomsRaw: any = adapter.rooms;
+
+	  for (let room of splittedRooms) {
+	  	  if (room.length > 0 && room[0] != '&'){
+	  	  	lastJoinedRoom = '&' + room;
+	  	  } else {
+	  	  	lastJoinedRoom = room;
+	  	  }
+		  let isUserAlreadyActiveInRoom: boolean = false;
+		  try {
+		  	const activeUsersInRoom: Array<ChatUser> = this.getActiveUsersInRoom(room);
+    		for (let i = 0; i < activeUsersInRoom.length; i++){
+  				if (client.id === activeUsersInRoom[i].client_id){
+  					isUserAlreadyActiveInRoom = true;
+  					break ;
+  				}
+  		    }
+		  } catch {}
+
+		  const successfulJoin = await 
+			this.joinUserToRoom(client, lastJoinedRoom, pass);
+
+		  if (successfulJoin){
+	 	  	const response: ChatMessage = {
+		  	    room: lastJoinedRoom,
+		  	    message: `you are in room ${lastJoinedRoom}`,
+		  	    nick: "system",
+		  	    date: new Date()
+		  	}
+			this.messageToClient(client.id, "join", response);
+			if (!isUserAlreadyActiveInRoom){ 
+				const oldMessagesInRoom: RoomMessages = 
+					await this.chatMessageService.getAllMessagesFromRoom(lastJoinedRoom);
+				for (let message of oldMessagesInRoom.messages){
+					this.messageToClient(client.id, "message", message)
+				}
+			}
+		  }
+	  }
   }
 }
--- frontend/src/src/app/pong/room/room.service.ts
+++ frontend/src/src/app/pong/room/room.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { SocketService } from '../../socket.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
+  private socketService: SocketService = new SocketService("/game");
   constructor() { }
+
+  joinUserToRoom(room: string){
+		this.socketService.sendMessageToServer("join", room);
+	}
+
+  partFromRoom(room: string){
+		this.socketService.sendMessageToServer("part", room);
+	}
+
+  makeRoomAdmin(nick: string, room: string){
+		//const payloadToSend: ChatMessage = { room: room, message: "" , nick: nick, date: new Date() }
+		//this.socketService.sendMessageToServer("admin", payloadToSend);
+	}
 }
+
+
