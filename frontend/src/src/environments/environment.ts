e74087cb457009653a467e615a8d33d45d9ec348
albgarci <albgarci@student.42madrid.com>
2023-07-16 11:48:32 +0200

    Feature: chat, announcing join to the rest of the room

--- frontend/src/src/environments/environment.ts
+++ frontend/src/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-   apiBaseUrl: 'http://10.11.2.6:3000',
-   apiUrl: 'http://10.11.2.6:3000',
-   frontendUrl: 'http://10.11.2.6:4200',
+   apiBaseUrl: 'http://localhost:3000',
+   apiUrl: 'http://localhost:3000',
+   frontendUrl: 'http://localhost:4200',
    clientId42: 'u-s4t2ud-8a6f2fa2b390840637041ee38b7e930881c496975d8123b200f71aac854c7c02'
 };
