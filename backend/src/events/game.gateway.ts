64a8a7d31aeb3eb5db562aa36a431eb508fc160b
Pablo Otero <potero91@gmail.com>
2023-07-17 19:07:16 +0200

    after two merges

--- backend/src/events/game.gateway.ts
+++ backend/src/events/game.gateway.ts
@@ -3,11 +3,17 @@ import { BaseGateway } from './base.gateway';
 import { Socket } from 'socket.io';
 import { ChatMessage, SocketPayload } from '@shared/types';
 import { RoomMessages, ChatUser } from '@shared/types';
+import { events, values } from '@shared/const';
+import { generateJoinResponse } from '@shared/functions';
+import { generateSocketErrorResponse, generateSocketInformationResponse } from '@shared/functions';
+import { UserService } from '../user/user.service';
+import { ChatMessageService } from '../chat/chat-message/chat-message.service';
+
 
 @WebSocketGateway({ namespace: '/game', cors: true } )
 export class GameGateway extends BaseGateway {
 
-  constructor() {
+  constructor(private userService: UserService, private chatMessageService: ChatMessageService) {
 	super(GameGateway.name);
   }
 
@@ -31,50 +37,82 @@ export class GameGateway extends BaseGateway {
   }
 
   @SubscribeMessage('join')
-  async handleJoinRoom(client: Socket, rooms: string): Promise<void>{
-//  	  console.log("join message received: " + rooms);
-	  const splittedRooms: Array<string> = rooms.split(" ", 1)[0].split(",");
-	  const pass: string | undefined = rooms.split(" ")[1];
-	  let lastJoinedRoom: string;
-	  const adapter: any = this.server.adapter;
-	  const roomsRaw: any = adapter.rooms;
+  async handleJoinRoom(client: Socket, roomAndPassword: string): Promise<void>{
+	  let room: string = roomAndPassword.split(" ", 2)[0];
+	  const pass: string | undefined = roomAndPassword.split(" ", 2)[1];
+	  const nick: string = client.handshake.query.nick as string;
 
-	  for (let room of splittedRooms) {
-	  	  if (room.length > 0 && room[0] != '&'){
-	  	  	lastJoinedRoom = '&' + room;
-	  	  } else {
-	  	  	lastJoinedRoom = room;
-	  	  }
-		  let isUserAlreadyActiveInRoom: boolean = false;
-		  try {
-		  	const activeUsersInRoom: Array<ChatUser> = this.getActiveUsersInRoom(room);
-    		for (let i = 0; i < activeUsersInRoom.length; i++){
-  				if (client.id === activeUsersInRoom[i].client_id){
-  					isUserAlreadyActiveInRoom = true;
-  					break ;
-  				}
-  		    }
-		  } catch {}
+	  for (const c of values.forbiddenChatRoomCharacters){
+		if (room.includes(c)){
+			this.server.to(client.id)
+				.emit("system", generateSocketErrorResponse(room, 
+					`Invalid name for the channel ${room}, try other`).data)
+			return ;
+		} 
+	  }
 
-		  const successfulJoin = await 
-			this.joinUserToRoom(client, lastJoinedRoom, pass);
+  	  if (room.length > 0 && room[0] != '#' && room[0] != '@'){
+  	  	room = '#' + room;
+  	  }
+  	  //check if user is banned from channel
+  	  await this.joinRoutine(client.id, nick, room, pass, "join")
+  }
 
-		  if (successfulJoin){
-	 	  	const response: ChatMessage = {
-		  	    room: lastJoinedRoom,
-		  	    message: `you are in room ${lastJoinedRoom}`,
-		  	    nick: "system",
-		  	    date: new Date()
-		  	}
-			this.messageToClient(client.id, "join", response);
-			if (!isUserAlreadyActiveInRoom){ 
-				const oldMessagesInRoom: RoomMessages = 
-					await this.chatMessageService.getAllMessagesFromRoom(lastJoinedRoom);
+  async joinRoutine(clientSocketId: string, nick: string, room: string, pass: string, typeOfJoin: string){
+	const originalRoom = room;
+		if (room.length > 0 && room[0] == '@'){
+			if (await this.userService
+					.isUserBannedFromUser(room.substr(1, room.length - 1), nick)){
+					return this.messageToClient(clientSocketId, "system", 
+						  generateSocketErrorResponse("", `You can't open a private conversation with ${room.substr(1, room.length - 1)} because you are banned`).data);
+	  		}	
+	  	room = await this.chatService.generatePrivateRoomName(nick, room.substr(1, room.length - 1))
+		}
+
+  		const wasUserAlreadyActiveInRoom: boolean = await this.isUserAlreadyActiveInRoom(clientSocketId, room);
+  		const successfulJoin = await 
+		  this.joinUserToRoom(clientSocketId, nick, room, pass);
+
+  		if (successfulJoin){
+			const response: ChatMessage = generateJoinResponse(originalRoom);
+			this.messageToClient(clientSocketId, typeOfJoin, response);
+
+			//sending old messages of the room, except for those of users that banned
+			//the new user trying to join
+			if (!wasUserAlreadyActiveInRoom){
+				let oldMessagesInRoom: RoomMessages = 
+					await this.chatMessageService.getAllMessagesFromRoom(room);
+
+			//get all u2u bans to 'nick'
+			//on every message, check the nick of the sender, if it's one of
+			//the users that have banned the one trying to join,
+			//the message isn't send
+				const usersThatHaveBanned: Array<string> = (await this.userService.getUsersThatHaveBannedAnother(nick)).map(u => u.nick)
+
+			//using originalRoom is a way to handle the names of private rooms:
+			//in db are #2-8, for instance, but we send @nick to the client as 
+			//a room name
+				if (originalRoom !== room){
+					oldMessagesInRoom.name = originalRoom
+					oldMessagesInRoom.messages.map(m => m.room = originalRoom)
+				}
 				for (let message of oldMessagesInRoom.messages){
-					this.messageToClient(client.id, "message", message)
+					if (!usersThatHaveBanned.includes(message.nick)){
+						this.messageToClient(clientSocketId, "message", message)
+					}
 				}
 			}
-		  }
-	  }
-  }
+  		}
+	}
+	async isUserAlreadyActiveInRoom(clientSocketId: string, room: string){
+		try {
+			const activeUsersInRoom: Array<ChatUser> = this.getActiveUsersInRoom(room);
+				for (let i = 0; i < activeUsersInRoom.length; i++){
+				  if (clientSocketId === activeUsersInRoom[i].client_id){
+					  return true;
+					}
+				}
+		} catch {}
+		return (false)
+	}
 }
