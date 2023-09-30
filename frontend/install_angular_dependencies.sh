9354c2662289c086e13f5d94b7a718dc1ebef293
Alberto García López <albgarci@student.42madrid.com>
2023-09-30 17:32:03 +0200

    Update: angular version to remove warnings

--- frontend/install_angular_dependencies.sh
+++ frontend/install_angular_dependencies.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-cd /usr/src/app
+cd /usr/src/app/src
 
 export NG_CLI_ANALYTICS="false"
 
-npm install -g npm@9.6.2
-npm install -g @angular/cli
-ng analytics off
+#npm install -g npm@9.6.2
+#npm install -g @angular/cli
 
-cd src
+ng analytics off
 npm install
+echo "eoooooo"
 npm run start
 #ng serve --host 0.0.0.0 --port 4200
